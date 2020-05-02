3a7fcbf56f28687e13325024b8751b1c4f2c7740
imasaki20 <imasaki20@gmail.com>
2020-05-02 16:05:39 +0900

    Fixed quicksort.py. Version 1.0

--- quicksort.py
+++ quicksort.py
@@ -1,19 +1,37 @@
 import random
 
+def med3(x, y, z):
+    if x < y:
+        if y < z:
+            return y
+        elif z < x:
+            return x
+        else:
+            return z
+    else:
+        if z < y:
+            return y
+        elif x < z:
+            return x
+        else:
+            return z
+
+
 def quicksort(target_list):
     return quicksort_sub(target_list, 0, len(target_list) - 1)
 
 
 def quicksort_sub(target_list, index_left, index_right):
     if index_left < index_right:
-        target_index = index_left + ((index_right - index_left) // 2)
-
         i = index_left
         j = index_right
+
+        p = med3(target_list[i], target_list[i + ((j - i) // 2)], target_list[j])
+
         while True:
-            while target_list[i] < target_list[target_index]:
+            while target_list[i] < p:
                 i += 1
-            while target_list[j] > target_list[target_index]:
+            while target_list[j] > p:
                 j -= 1
             
             if i >= j:
@@ -23,8 +41,8 @@ def quicksort_sub(target_list, index_left, index_right):
             i += 1
             j -= 1
 
-        target_list[index_left:i - 1] = quicksort_sub(target_list, index_left, i - 1)[index_left:i - 1]
-        target_list[j + 1:index_right] = quicksort_sub(target_list, j + 1, index_right)[j + 1:index_right]
+        target_list = quicksort_sub(target_list, index_left, i - 1)
+        target_list = quicksort_sub(target_list, j + 1, index_right)
 
     return target_list
 
